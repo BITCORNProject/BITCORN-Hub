3cb33ab4f6bf5c160c348e23c4556f174600b3d3
Jones S. Croppper <callowcreation@gmail.com>
2019-08-16 17:02:18 -0700

    activity tracker stores user object and increments count to allow for more users for rain

--- settings/server-settings.js
+++ settings/server-settings.js
@@ -8,6 +8,6 @@ const JsonFile = require('../source/utils/json-file');
 
 module.exports = new JsonFile('./settings/server-settings.json', {
     IRC_DELAY_MS: 100,
-    MAX_RAIN_USER_CACHE: 25 * 1.4, // 35
-    MINUTE_AWARD_MULTIPLIER: 4.3333333333 // 4 mins 20 secs
+    MAX_RAIN_USER_CACHE: 25,
+    MINUTE_AWARD_MULTIPLIER: 4.3333333333
 });
