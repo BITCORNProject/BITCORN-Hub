9bcaa6bf732d4bee94b69d81037598629c1b4c17
Jones S. Croppper <callowcreation@gmail.com>
2021-09-09 11:40:19 -0700

    timeouts between services startup

--- start all.bat
+++ start all.bat
@@ -1,5 +1,9 @@
-start node _settings-service/app
 start node _data-service/app
+TIMEOUT /T 5 /NOBREAK
+start node _settings-service/app
+TIMEOUT /T 5 /NOBREAK
 start node _twitch-service/app
+TIMEOUT /T 5 /NOBREAK
 start node _bot-service/app
+TIMEOUT /T 2 /NOBREAK
 cmd /k
