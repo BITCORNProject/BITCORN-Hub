64db22aba84dd05bbf379d747ae4840930d29cb7
Jones S. Croppper <callowcreation@gmail.com>
2021-09-23 16:17:34 -0700

    join channel by finding username that changed by accessing the Twitch api to get the new name from the user id

--- _bot-service/src/room-visitor.js
+++ _bot-service/src/room-visitor.js
@@ -3,12 +3,15 @@
 const _ = require('lodash');
 const Queue = require('./utils/queue');
 const databaseApi = require('../../_api-shared/database-api');
+const settingsHelper = require('../settings-helper');
+const { getIds } = require('./request-api');
 
 const SETTINGS_JOIN_LEAVE_INTERVAL_MS = 1000 * 60 * 2;
 const MAX_JOIN_RETRIES = 5;
 let retrieCount = 0;
 
 const joinedChannels = [];
+const nameChangeChannelMap = {};
 
 function _Queue() {
 	this.items = new Queue();
@@ -19,12 +22,13 @@ function _Queue() {
 	this.peek = function () { return this.items.peek(); }
 	this.enqueue = function (item) { return this.items.enqueue(item); }
 	this.dequeue = function () { return this.items.dequeue(); }
+	this.replace = function (index, value ) { this.items.items.splice(index, 1, value); }
 }
 
 const queuedItems = new _Queue();
 
 async function partChannels(tmi, leaves) {
-	if(leaves.length === 0) return;
+	if (leaves.length === 0) return;
 	for (let i = 0; i < leaves.length; i++) {
 		const result = await tmi.partChannel(leaves[i]);
 		console.log({ left_channel: result.join() });
@@ -53,7 +57,7 @@ function getJoinQueue() {
 async function joinChannelsFromQueue(tmi) {
 
 	if (queuedItems.size() === 0) return { message: 'Empty Queue' };
-	
+
 	if (queuedItems.isBusy === true) return { message: `join queue is busy with size: ${queuedItems.size()}` };
 
 	queuedItems.isBusy = true;
@@ -74,20 +78,38 @@ async function joinChannelsFromQueue(tmi) {
 		result = { message: joined };
 	} catch (error) {
 		retrieCount++;
-		if (retrieCount === MAX_JOIN_RETRIES) {
+
+		if (error === 'msg_banned' || error === 'msg_channel_suspended') {
 			queuedItems.dequeue();
 			retrieCount = 0;
 			result = { message: error, item, maxRetries: true };
-		} else {
+		} else if (error === 'No response from Twitch.') {
+
+			const channel_id = settingsHelper.getProperty(item, 'ircTarget');
+			const { data } = await getIds([channel_id]);
+			// console.log(data);
+
+			nameChangeChannelMap[item] = data[0].login;
+			queuedItems.replace(0, data[0].login);
+
 			result = { message: error, item, maxRetries: false };
+
+		} else {
+			if (retrieCount === MAX_JOIN_RETRIES) {
+				queuedItems.dequeue();
+				retrieCount = 0;
+				result = { message: error, item, maxRetries: true };
+			} else {
+				result = { message: error, item, maxRetries: false };
+			}
 		}
 	}
-	
+
 	queuedItems.isBusy = false;
 
 	joinChannelsFromQueue(tmi);
 
-	console.log({result, timestamp: new Date().toLocaleTimeString()});
+	console.log({ result, timestamp: new Date().toLocaleTimeString() });
 
 	return result;
 }
@@ -98,6 +120,10 @@ module.exports = async (tmi) => {
 
 		const channels = await databaseApi.makeRequestChannels();
 
+		// // ----- REMOVE TEST CODE
+		// channels.push('likeweedtv420');
+		// // ----- REMOVE TEST CODE
+
 		addChannels(channels);
 		joinChannelsFromQueue(tmi);
 
@@ -107,7 +133,18 @@ module.exports = async (tmi) => {
 
 	setInterval(async () => {
 		try {
-			const channels = await databaseApi.makeRequestChannels();
+			let channels = await databaseApi.makeRequestChannels();
+
+			// // ----- REMOVE TEST CODE
+			// channels.push('likeweedtv420');
+			// // ----- REMOVE TEST CODE
+
+			channels = channels.map(x => {
+				if(Object.keys(nameChangeChannelMap).includes(x)) {
+					return nameChangeChannelMap[x];
+				}
+				return x;
+			});
 
 			const leaves = _.difference(joinedChannels, channels);
 			const joins = _.difference(channels, joinedChannels);
